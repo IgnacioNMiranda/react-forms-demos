437f4a1cff7ab126108040d082aa834812162950
IgnacioNMiranda <i.miranda.f99@gmail.com>
2022-07-10 10:27:54 -0400

    feat: hook form implementation

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,32 @@ import { finalFormControlsMock, formikControlsMock, hookFormControlsMock, native
 
 function App() {
   const onSubmit: OrFormProps['handleSubmit'] = (values) => {
-    // This would send data to some service based on a serviceType prop or whatever.
-    alert(JSON.stringify(values))
+    // Here would send data to some service based on a serviceType prop or whatever.
+    alert(JSON.stringify(values, null, 2))
   }
 
   return (
     <main className="min-h-screen w-full flex flex-col justify-center items-center">
-      <h1 className="pt-4 pb-20 lg:pb-40 text-4xl text-slate-700 font-bold">⚛️ React Forms Demos 📃</h1>
-      <div className="w-full max-w-7xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 px-8 py-4">
-        <div className="flex flex-col gap-2">
+      <h1 className="pt-4 pb-20 lg:pb-40 text-4xl text-slate-700 font-bold text-center px-8">
+        ⚛️ React Forms Demos 📃
+      </h1>
+      <div className="w-full flex flex-wrap justify-center gap-10 px-8 py-4">
+        <div className="flex flex-col gap-2 w-full sm:w-64">
           <h2 className="text-center uppercase text-slate-600 font-bold">Native Form</h2>
           <OrForm type={OrFormType.NATIVE} controls={nativeControlsMock} handleSubmit={onSubmit} />
         </div>
 
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-full sm:w-64">
           <h2 className="text-center uppercase text-slate-600 font-bold">Formik Form</h2>
           <OrForm type={OrFormType.FORMIK} controls={formikControlsMock} handleSubmit={onSubmit} />
         </div>
 
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-full sm:w-64">
           <h2 className="text-center uppercase text-slate-600 font-bold">Final Form</h2>
           <OrForm type={OrFormType.FINAL_FORM} controls={finalFormControlsMock} handleSubmit={onSubmit} />
         </div>
 
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 w-full sm:w-64">
           <h2 className="text-center uppercase text-slate-600 font-bold">Hook Form</h2>
           <OrForm type={OrFormType.HOOK_FORM} controls={hookFormControlsMock} handleSubmit={onSubmit} />
         </div>
--- src/components/ml-form-field/index.tsx
+++ src/components/ml-form-field/index.tsx
@@ -1,4 +1,4 @@
-import { FocusEventHandler, InputHTMLAttributes, memo, useState } from 'react'
+import { FocusEventHandler, forwardRef, InputHTMLAttributes, useState } from 'react'
 
 export type MlFormFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   errorMessage?: string
@@ -6,54 +6,60 @@ export type MlFormFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   id: string
 }
 
-export const MlFormField = ({
-  id,
-  type,
-  name,
-  placeholder,
-  value,
-  onChange,
-  onFocus,
-  onBlur,
-  errorMessage,
-  visited,
-  required = false,
-}: MlFormFieldProps) => {
-  const hasError = !!errorMessage && visited
+export const MlFormField = forwardRef<HTMLInputElement, MlFormFieldProps>(
+  (
+    {
+      id,
+      type,
+      name,
+      placeholder,
+      value,
+      onChange,
+      onFocus,
+      onBlur,
+      errorMessage,
+      visited,
+      required = false,
+    }: MlFormFieldProps,
+    ref,
+  ) => {
+    const hasError = !!errorMessage && visited
 
-  const [isFocus, setIsFocus] = useState(false)
-  const handleFocus: FocusEventHandler<HTMLInputElement> = (e) => {
-    onFocus?.(e)
-    setIsFocus(true)
-  }
+    const [isFocus, setIsFocus] = useState(false)
+    const handleFocus: FocusEventHandler<HTMLInputElement> = (e) => {
+      onFocus?.(e)
+      setIsFocus(true)
+    }
 
-  const handleBlur: FocusEventHandler<HTMLInputElement> = (e) => {
-    onBlur?.(e)
-    setIsFocus(false)
-  }
+    const handleBlur: FocusEventHandler<HTMLInputElement> = (e) => {
+      onBlur?.(e)
+      setIsFocus(false)
+    }
 
-  return (
-    <div className="flex flex-col gap-y-2 max-w-full">
-      <div
-        className={`border px-4 py-2 rounded-sm transition-colors ${hasError ? 'border-red-400' : ''} ${
-          isFocus ? 'border-cyan-700' : 'border-gray-300'
-        }`}
-      >
-        <input
-          className="focus:outline-none w-full"
-          id={id}
-          type={type}
-          name={name}
-          required={required}
-          placeholder={`${placeholder}${required ? ' (*)' : ''}`}
-          onChange={onChange}
-          onBlur={handleBlur}
-          onFocus={handleFocus}
-          value={value}
-          autoComplete={type === 'password' ? 'off' : 'on'}
-        />
+    return (
+      <div className="flex flex-col gap-y-2 max-w-full">
+        <div
+          className={`border px-4 py-2 rounded-sm transition-colors ${hasError ? 'border-red-400' : ''} ${
+            isFocus ? 'border-cyan-700' : 'border-gray-300'
+          }`}
+        >
+          <input
+            className="focus:outline-none w-full"
+            id={id}
+            type={type}
+            ref={ref}
+            name={name}
+            placeholder={`${placeholder}${required ? ' (*)' : ''}`}
+            onChange={onChange}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+            value={value}
+            autoComplete={type === 'password' ? 'off' : 'on'}
+          />
+        </div>
+        {hasError && <span className="text-red-400 font-bold">{errorMessage}</span>}
       </div>
-      {hasError && <span className="text-red-400 font-bold">{errorMessage}</span>}
-    </div>
-  )
-}
+    )
+  },
+)
+MlFormField.displayName = 'MlFormField'
